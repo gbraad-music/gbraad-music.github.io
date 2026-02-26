58d2c3b6770209937e3926bc4468e21e7adbad34
Gerard Braad <me@gbraad.nl>
2026-02-26 17:56:40 +0800

    Add PWA

---
+++ microaudio/service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'microaudio-controller-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './pad-knob.js',
+  './controller.js',
+  './manifest.json',
+  './favicon.svg',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[SW] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
